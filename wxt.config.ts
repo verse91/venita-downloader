e9ad4917034f53e8611eaf535e79389a9242f415
VERSE <sunnyone0901@gmail.com>
2025-07-26 16:45:37 +0700

    feat: add manifest configuration for Venita extension with necessary permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,22 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  manifest: ({ browser, manifestVersion, mode, command }) => {
+    return {
+      name: "Venita",
+      description: "An extension to download clips from specific time range ",
+      version: "1.0.0",
+      manifest_version: 3,
+      permissions: [
+        "storage",
+        "activeTab",
+        "tabs",
+        "contextMenus",
+        "scripting",
+        "declarativeNetRequest",
+      ],
+    };
+  },
 });
